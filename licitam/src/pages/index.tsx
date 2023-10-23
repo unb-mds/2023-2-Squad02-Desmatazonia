d4a0fa10d849b1cc1887c454c8be8cacdd10bbc2
Angelicahaas <211061832@aluno.unb.br>
2023-10-23 12:10:49 -0300

    adicionando fontes

--- licitam/src/pages/index.tsx
+++ licitam/src/pages/index.tsx
@@ -3,53 +3,46 @@ import Navbar from '@/components/_navbar';
 import Cob from '@/components/_cob';
 import Municipios from '@/components/_municipios';
 import Anos from '@/components/_anos';
-import { Inter, Source_Sans_3 } from 'next/font/google'
+import { Inter, Source_Sans_3 } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 const Home = () => {
   return (
     <div className="container">
       <div className="navbar">
-        <div className=''>
+        <div className=""></div>
+        <Navbar />
+      </div>
 
-        </div>
-        <Navbar /> {}
-      </div>
-      
       <div className="cob">
-        <Cob /> {/* Componente Cob à esquerda */}
+        <Cob />
       </div>
-      <div className="right-column">
+
+      <div className="text-center">
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <div className=" text-center">
+            <p className="h1 custom-title">
               Acompanhe as licitações dos Municípios de Amazonas!
             </p>
           </div>
 
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-          </div>
 
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <a
-            >
-              <h2>
-                Reunimos os Diários Oficiais Municipais do Estado do Amazonas, disponibilizados pela Associação dos Municípios Amazonenses (AMA). Ao selecionar uma cidade e período específico, forneceremos informações sobre as licitações realizadas nessa região. Escolha o município desejado e explore as oportunidades de negócios públicos na Amazônia!
+          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4t">
+            <a>
+              <h2 className="font-inria-serif text-center">
+                   Reunimos os Diários Oficiais Municipais do Estado do Amazonas, disponibilizados pela Associação dos Municípios Amazonenses (AMA). Ao selecionar uma cidade e período específico, forneceremos informações sobre as licitações realizadas nessa região. Escolha o município desejado e explore as oportunidades de negócios públicos na Amazônia!
               </h2>
             </a>
-            
           </div>
 
-          <div className='municipio-box'>
-            <div className='center-box'>
-              <Municipios /> {}
+
+          <div className="box-container">
+            <div className="box">
+              <Municipios />
             </div>
-          </div>
-          
-          <div className='anos-box'>
-            <div className='center-box'>
-              <Anos /> {}
+            <div className="box">
+              <Anos />
             </div>
           </div>
         </main>
@@ -62,14 +55,30 @@ const Home = () => {
         }
 
         .left-column {
-          width: 30%; /* Define a largura da coluna esquerda */
-          background-color: gray; /* Cor de fundo cinza */
-          padding: 20px; /* Espaçamento interno */
+          width: 30%;
+          background-color: gray;
+          padding: 20px;
         }
 
         .right-column {
-          flex: 1; /* A coluna da direita ocupa o espaço restante */
-          padding: 20px; /* Espaçamento interno */
+          flex: 1;
+          padding: 20px;
+        }
+
+        .box-container {
+          display: flex;
+          flex-wrap: wrap; /* Para que as caixas possam quebrar para a próxima linha quando não houver espaço suficiente na largura */
+        }
+
+        .box {
+          width: 305px;
+          height: 51px;
+          padding: 20px;
+          margin: 10px;
+          flex-shrink: 0;
+          border-radius: 20px;
+          background: linear-gradient(to left, #7265ba, #b9b1ec, #d0cce9);
+          display: inline-block;
         }
       `}</style>
     </div>

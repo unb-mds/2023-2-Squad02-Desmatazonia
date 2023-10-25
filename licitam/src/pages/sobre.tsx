27f2b19b68ef711fdff381ead0f5cc8113ad6f4d
IsaqueSH <isaquefrasciawerner@gmail.com>
2023-10-25 02:10:47 -0300

    Ajuste do arquivo sobre.tsx

--- licitam/src/pages/sobre.tsx
+++ licitam/src/pages/sobre.tsx
@@ -20,26 +20,33 @@ const Home = () => {
       <div className=" projeto">
         <div className="text-center">
           <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`flex min-h-screen flex-col items-center justify-between p-15 ${inter.className}`}
           >
-            <div className=" text-center">
-              <p className="h1 custom-title">Sobre o nosso Projeto</p>
+            <div className="text-center">
+              <h1 className="h1 custom-title">Sobre o nosso Projeto</h1>
             </div>
 
-            <div className=" text-center lg:max-w-5xl  lg:mb-0 lg:grid-cols-4t">
+            <div className="text-center lg:max-w-3xl lg:mb-0 lg:grid-cols-4t">
               <a>
-                <h2>
+                <p className="aumentar-fonte">
                   O propósito deste projeto é reunir as informações das
                   licitações realizadas pelos municípios do Amazonas e
                   apresentá-las de maneira transparente à sociedade, organizando
                   os dados em gráficos que destacam os anos e os respectivos
                   municípios.
-                </h2>
+                </p>
               </a>
             </div>
 
             <div>
               <img src="/images/mapa_ama.jpg" />
+            </div>
+            <div className="text-center lg:max-w-7xl lg:mb-0 lg:grid-cols-5t">
+              <h1>Extrator de dados</h1>
+              <p className="aumentar-fonte">
+                Nosso site e extrator funcionam da seguinte maneira: <br></br>
+                O usuário seleciona o município e o ano. O extrator busca em documentos PDF do Diário Oficial, usando o Apache Tika para extrair texto. Em seguida, fazemos pós-processamento para limpar e formatar o texto. Por fim, apresentamos os resultados para visualização e download das informações sobre licitações.
+              </p>
             </div>
           </main>
         </div>
@@ -50,10 +57,24 @@ const Home = () => {
           display: flex;
           width: 100%;
         }
+        p {
+          color: black;
+        }
+        img {
+          width: 600px;
+          height: 350px;
+        }
+        h1 {
+          color: black;
+        }
+
+        .aumentar-fonte {
+          font-size: 24px; /* Aumente o tamanho da fonte conforme necessário */
+        }
 
         .box-container {
           display: flex;
-          flex-wrap: wrap; /* Para que as caixas possam quebrar para a próxima linha quando não houver espaço suficiente na largura */
+          flex-wrap: wrap;
         }
 
         .box {

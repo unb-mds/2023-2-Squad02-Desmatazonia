0d4106dad9318dd21c33edac118966c1bbe0c1c9
acamposs <amanda.acampos2000@gmail.com>
2023-11-28 00:26:45 -0300

    corrigi extrator de desmatamento

--- desmatamento/extrator_desmatamento.py
+++ desmatamento/extrator_desmatamento.py
@@ -2,8 +2,7 @@ import json
 import os
 import pandas as pd
 
-# Carrega a base de dados a partir de um arquivo CSV
-# Substitua pelo caminho real do seu arquivo CSV
+# Carrega a base de dados CSV
 csv_path = "desmatamento/amazonas_desmatamento.csv"
 dados_desmatamento = pd.read_csv(csv_path, encoding="utf-8")
 
@@ -17,12 +16,10 @@ os.makedirs(path_json, exist_ok=True)
 dados_agrupados = dados_desmatamento.groupby('municipio').apply(
     lambda x: x.drop('municipio', axis=1).to_dict('records')).to_dict()
 
-# Loop para criar arquivos JSON para cada município
+#  criar arquivos JSON para cada município
 for nome_municipio, info_municipios in dados_agrupados.items():
-    # Cria o caminho do arquivo para o município
     file_path = os.path.join(path_json, f"{nome_municipio}.json")
 
-    # Salva as informações do município em um arquivo JSON
     with open(file_path, "w", encoding="utf-8") as json_file:
         json.dump(info_municipios, json_file, indent=2, ensure_ascii=False)
 

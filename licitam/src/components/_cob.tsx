5e49ddf293f31c618ad54f772a6e5d84f0b76897
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2023-12-09 22:30:40 -0300

    ci/cd

--- licitam/src/components/_cob.tsx
+++ licitam/src/components/_cob.tsx
@@ -3,21 +3,20 @@ import React from "react";
 const Cob = () => {
   return (
     <main className="min-h-screen h-[100%]">
-      <div className="flex flex-col items-center justify-between h-[80%] mr-2 ">
+      <div className="flex flex-col items-center justify-evenly h-[80%] mr-2 ">
         <div>
           <h1 className="text-center text-4xl mt-10 font-[PoppinsMedium]">
             Desmata<span className="text-green-500">Zônia</span>
           </h1>
-          <p className="text-center text-sm font-[PoppinsRegular]">
-            Desmatamentos do Estado de Amazonas no seu alcance
-          </p>
-
-          <div className="flex flex-col items-center mt-8 ">
-            <p className="text-center font-[PoppinsLight]">Colaboradores</p>
-            <img src="/images/colaboradores.png" className="w-72" />
+          <div className="flex items-center justify-center">
+            <img className="w-[14rem]" src="/images/logo-desmatazonia.png" />
           </div>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-center text-xl mt-8 font-[PoppinsSemiBold]">
+          {/* <p className="text-center text-sm font-[PoppinsRegular]">
+            Desmatamentos do Estado de Amazonas no seu alcance
+          </p> */}
+
+          {/* <div className="flex flex-col gap-2">
+            <h1 className="text-center text-xl mt-3 font-[PoppinsSemiBold]">
               O que é Desmatamento?
             </h1>
             <p className="text-center text-base pl-6 font-[PoppinsLight]">
@@ -26,18 +25,22 @@ const Cob = () => {
               produtivas como: agricultura, pecuária, exploração madeireira,
               mineração e infraestrutura e urbana.
             </p>
-          </div>
+          </div> */}
 
-          <p className="text-center text-base font-[PoppinsRegular] mt-10">
-            Coletamos e Organizamos os Dados <br /> dos{" "}
-            <span className="text-green-500">62 municípios</span> do Amazonas
-            <br />
-            para Impactar a Transparência e Eficiência <br /> dos desmatamentos
-            Municipais.
+          <p className="text-center text-base font-[PoppinsRegular] ">
+            Coletamos e Organizamos <br />
+            os Dadosdos <span className="text-green-500">
+              62 municípios
+            </span>{" "}
+            <br /> do Amazonas para Impactar a <br /> Transparência e Eficiência{" "}
+            <br /> dos desmatamentos Municipais.
           </p>
         </div>
 
-        <img className="w-[12rem] " src="/images/mapa_ama_cob.png" />
+        <div className="flex flex-col items-center">
+          <p className="text-center font-[PoppinsLight]">Colaboradores</p>
+          <img src="/images/colaboradores.png" className="w-72" />
+        </div>
       </div>
     </main>
   );
